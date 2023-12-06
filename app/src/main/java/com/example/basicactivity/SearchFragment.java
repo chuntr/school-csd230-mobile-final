0e8217abff9cda859d17bcf9a9e8ef0bf91cde93
Christopher Hunter <chuntr@users.noreply.github.com>
2023-12-05 20:38:59 -0800

    added preliminary REST call testing, works

--- app/src/main/java/com/example/basicactivity/SearchFragment.java
+++ app/src/main/java/com/example/basicactivity/SearchFragment.java
@@ -4,11 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.basicactivity.databinding.FragmentSearchBinding;
 
 public class SearchFragment extends Fragment {
@@ -28,11 +35,38 @@ public class SearchFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        TextView textView = binding.textView;
+
+        //
         binding.buttonSearch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                RequestQueue queue = Volley.newRequestQueue(getActivity().getApplicationContext());
+                String url = "https://api.nal.usda.gov/fdc/v1/foods/search?api_key=JQ5aRpzEtKywc1sS6YYf4HcGfY0fpMLBULBJ1PcW&query=tomato";
+
+                //TODO: add url params, change to post
+                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                textView.setText("Response is" + response);
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work");
+                    }
+                });
+
+                // add this to the request queue
+                queue.add(stringRequest);
+
+                /*
                 NavHostFragment.findNavController(SearchFragment.this)
                         .navigate(R.id.action_SearchFragment_to_ResultsFragment);
+
+                */
             }
         });
     }

49c4b93ef8dcf13708a2eb15ff5a9f9eb371ffc0
Christopher Hunter <chuntr@users.noreply.github.com>
2023-12-07 07:21:44 -0800

    enabling menu items

--- app/src/main/java/com/example/basicactivity/MainActivity.java
+++ app/src/main/java/com/example/basicactivity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.basicactivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -32,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -42,19 +43,14 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
-    */
 
     @Override
     public boolean onSupportNavigateUp() {
